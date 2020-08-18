2ff7de4fb547f5ba54ba623773a6e26718abc863
Prince-Hamza <38145860+Prince-Hamza@users.noreply.github.com>
2020-08-17 22:44:05 -0700

    Add files via upload

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,37 +1,38 @@
 import React, { Component } from "react";
-import {options} from './Options'
+import {options} from './Options.js'
 import * as parser from 'fast-xml-parser'
 
 export default class Main extends Component {
   componentDidMount = () => {
 
-  const Xml = `<note>
-                  <to>Ronny</to>
-                  <from>Humza</from>
-                  <heading>Reminder</heading>
-                  <body>Don't forget me this weekend!</body>
-               </note>`
 
-  if (parser.validate(Xml) === true) { 
-    var jsonObj = parser.parse(Xml ,options);
-   }
+  const Xml = `<note><to>Ronny</to>from>Humza</from></note>`
 
-
-   document.getElementById("Result").value = JSON.stringify(jsonObj);
-
-   
-
+ 
   }
   constructor(props) {
     super(props);
     this.state = {
-      
+      Xml:``      
     };
   }
   
   render() {
     return (
       <div>
+        <input id = "InputXml" style = {InputXml} onChange = {(e)=>{
+          
+          var Xml = e.target.value
+        //  alert(e.target.value)
+          if (parser.validate(Xml) === true) { 
+            var jsonObj = parser.parse(Xml ,options);
+           }
+        
+           document.getElementById("Result").value = JSON.stringify(jsonObj);
+
+
+          }} />
+
         <input id = "Result" type = "text"  style = {verstyle} />
       </div>
     );
@@ -41,10 +42,30 @@ export default class Main extends Component {
 
 let verstyle = {
   position:'absolute' ,
-  top:'10%' ,
+  top:'75%' ,
   left:'0%' ,
-  width:"100%" , "height":"60%" ,
+  width:"100%" , height:"60%" ,
   backgroundColor: "#222" , 
   color:"cyan" , 
   font : "bold italic 16px times new roman"
 }
+
+let buttonStyle = {
+  position:'absolute' ,
+  top:'75%' ,
+  left:'0%' ,
+  width:"20%" , height:"10%" ,
+  backgroundColor: "#222" , 
+  color:"cyan" , 
+  font : "bold italic 16px times new roman"
+}
+
+let InputXml = {
+  position:'absolute' ,
+  top:'10%' ,
+  left:'0%' ,
+  width:"100%" , height:"60%" ,
+  backgroundColor: "#222" , 
+  color:"cyan" , 
+  font : "bold italic 16px times new roman"
+} 
